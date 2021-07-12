f9b1dbe253e735635f1abf8e8ac626030a3a26b2
TimonDalton <timondalton2001@gmail.com>
2021-07-12 20:52:02 +0200

    Gui is almost functional

---
+++ src/lib/Equation.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { i } from 'mathjs';
+import '../solver/equation';
+	export let equation;
+
+</script>
+
---
+++ src/solver/equation.js
@@ -0,0 +1,69 @@
+import * as math from 'mathjs';
+
+export function getVarsFromTree(node){
+    let functions = ['sin', 'asin', 'cos', 'acos', 'tan', 'atan', 'ln'];
+    let vars = [];
+    node.traverse(function (node) {
+        switch (node.type) {
+          case 'SymbolNode':
+            if(!vars.includes(node.name)&&!functions.includes(node.name))
+            vars.push(node.name);
+            break
+          default:
+        }
+      });
+    return vars;
+}
+
+export function createEquation(inputString){
+    let equation = {
+        unknownVars:-1,
+        leftNode:null,
+        rightNode:null,
+        localVariables:[],
+        localScope:{},
+        inputString:inputString,
+        init:function(){
+            if(this.inputString == ""){console.log(inputString);return;}
+            console.log("initialIndexOf(=): "+ this.inputString.indexOf('='));
+            if(this.inputString.indexOf('=')>0){
+
+                console.log("inputString: "+this.inputString);
+                let leftStr = this.inputString.substring(0,this.inputString.indexOf('='));
+                console.log("left node: "+leftStr);
+                // console.log(this.leftNode);
+                let rightStr = this.inputString.substring(this.inputString.indexOf('=')+1,this.inputString.length)
+                console.log("right node: "+rightStr);
+                // console.log(this.rightNode);
+
+                this.leftNode = math.parse(leftStr);
+                this.rightNode = math.parse(rightStr);
+                
+
+                let singleNodeRepresentation = new math.OperatorNode('-','subtract',[this.leftNode,this.rightNode]);
+                this.localVariables = getVarsFromTree(singleNodeRepresentation);
+                console.log(singleNodeRepresentation);
+                console.log("local vars: ");
+                console.log(this.localVariables);
+                // console.log("Local var count: "+this.localVariables.length);
+                this.unknownVars = this.localVariables.length;
+            }  
+            return this;
+        },
+        setInputString:function(inStr){
+            this.inputString = inStr;
+            this.init();
+            this.updateLocalScope(this.localScope);
+        },
+        updateLocalScope:function(scope){
+            let keys = Object.keys(scope);
+            for(let i =0;i<keys.length;i++){
+                if(keys[i] in this.localVariables){
+                    this.localScope[keys[i]] = scope[keys[i]];
+                    unknownVars--;
+                }
+            }
+        }
+    }
+    return equation;
+}
